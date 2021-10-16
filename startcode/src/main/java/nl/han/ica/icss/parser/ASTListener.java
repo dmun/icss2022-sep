948a086cbfa9167e502acb810c1547c8aeee8836
David van Munster <davidru01@live.nl>
2021-10-16 20:40:26 +0200

    Change ASTListener for parseTest0

--- startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
+++ startcode/src/main/java/nl/han/ica/icss/parser/ASTListener.java
@@ -3,6 +3,7 @@ package nl.han.ica.icss.parser;
 import java.util.Stack;
 
 
+import nl.han.ica.datastructures.HANStack;
 import nl.han.ica.datastructures.IHANStack;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.*;
@@ -26,10 +27,105 @@ public class ASTListener extends ICSSBaseListener {
 
 	public ASTListener() {
 		ast = new AST();
-		//currentContainer = new HANStack<>();
+		currentContainer = new HANStack<>();
 	}
+
     public AST getAST() {
         return ast;
     }
-    
+
+	@Override
+	public void enterStylesheet(ICSSParser.StylesheetContext ctx) {
+		ASTNode stylesheet = new Stylesheet();
+		currentContainer.push(stylesheet);
+	}
+
+	@Override
+	public void exitStylesheet(ICSSParser.StylesheetContext ctx) {
+		ast.setRoot((Stylesheet) currentContainer.pop());
+	}
+
+	@Override
+	public void enterStyle_rule(ICSSParser.Style_ruleContext ctx) {
+		var styleRule = new Stylerule();
+		currentContainer.push(styleRule);
+	}
+
+	@Override
+	public void exitStyle_rule(ICSSParser.Style_ruleContext ctx) {
+		var styleRule = currentContainer.pop();
+		currentContainer.peek().addChild(styleRule);
+	}
+
+	@Override
+	public void enterSelector(ICSSParser.SelectorContext ctx) {
+		ASTNode selector;
+		var text = ctx.getText();
+		switch (text.charAt(0)) {
+			case '.':
+				selector = new ClassSelector(text);
+				break;
+			case '#':
+				selector = new IdSelector(text);
+				break;
+			default:
+				selector = new TagSelector(text);
+				break;
+		}
+		currentContainer.push(selector);
+	}
+
+	@Override
+	public void exitSelector(ICSSParser.SelectorContext ctx) {
+		var selector = currentContainer.pop();
+		currentContainer.peek().addChild(selector);
+	}
+
+	@Override
+	public void enterDeclaration(ICSSParser.DeclarationContext ctx) {
+		var declaration = new Declaration(ctx.getText());
+		currentContainer.push(declaration);
+	}
+
+	@Override
+	public void exitDeclaration(ICSSParser.DeclarationContext ctx) {
+		var declaration = currentContainer.pop();
+		currentContainer.peek().addChild(declaration);
+	}
+
+	@Override
+	public void enterProperty_name(ICSSParser.Property_nameContext ctx) {
+		var propertyName = new PropertyName(ctx.getText());
+		currentContainer.push(propertyName);
+	}
+
+	@Override
+	public void exitProperty_name(ICSSParser.Property_nameContext ctx) {
+		var propertyName = currentContainer.pop();
+		currentContainer.peek().addChild(propertyName);
+	}
+
+	@Override
+	public void enterLiteral(ICSSParser.LiteralContext ctx) {
+		ASTNode literal;
+		var text = ctx.getText();
+		if (text.startsWith("#")) {
+			literal = new ColorLiteral(text);
+		} else if (text.endsWith("px")) {
+			literal = new PixelLiteral(text);
+		} else if (text.endsWith("%")) {
+			literal = new PercentageLiteral(text);
+		} else {
+			literal = new ScalarLiteral(text);
+		}
+		currentContainer.push(literal);
+	}
+
+
+	@Override
+	public void exitLiteral(ICSSParser.LiteralContext ctx) {
+		var literal = currentContainer.pop();
+		currentContainer.peek().addChild(literal);
+	}
+
 }
